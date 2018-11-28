9c443c69161906300e586722ee92b7b0cd76c092
Connor Teh-Hall <connor.tehhall@hotmail.co.nz>
2018-11-28 16:43:43 +1300

    username commit

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-
     return render_template('home.html')
+
+
+@app.route('/user/<username>')
+def user(username):
+    return "Hey there %s" % username
 
 
 if __name__ == '__main__':
